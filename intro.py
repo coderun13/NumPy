daf89a5101c59afb7157db096f2ff60a20cfc896
snehal singh <snehalsingh.0407@gmail.com>
2026-04-22 23:58:13 +0530

    add file

---
+++ intro.py
@@ -0,0 +1,13 @@
+# import numpy as np
+
+# array in numpy
+# arr = np.array([1,2,3,4,5])
+# print(arr)
+# print(type(arr))
+
+# tuple in numpy
+import numpy as np
+arr = np.array((1,2,3,4,5))
+print(arr)
+print(type(arr))
+
