7ce3e6aeed361f51f55415cd5393889880925c68
snehal singh <snehalsingh.0407@gmail.com>
2026-04-29 00:20:02 +0530

    dimensions in array

--- intro.py
+++ intro.py
@@ -11,3 +11,14 @@ arr = np.array((1,2,3,4,5))
 print(arr)
 print(type(arr))
 
+# dimensions in array
+arr = np.array(20)
+arr1 = np.array([[1,2],[3,4]])
+print(arr)
+print(type(arr))
+print(arr.ndim)
+print(arr1.ndim)
+
+
+
+
